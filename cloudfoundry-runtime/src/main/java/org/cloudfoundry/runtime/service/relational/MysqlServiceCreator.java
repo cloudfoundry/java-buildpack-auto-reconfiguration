6ea9b0cc50b181e1f8ae95bd54588b9f08b13319
Scott Andrews <andrewss@vmware.com>
2011-05-20 16:04:26 -0400

    Add a validation query for MySQL connection pools, testing on borrow.
    For Java applications, MySQL connections in the DBCP pool currently timeout
    after (approximately 8) hours of inactivity.  The first request that needs a
    live connection will fail.  In order to ensure that all connections dispensed
    by the pool are valid, we are adding a validation query that is tested before
    the connection is retrieved from the pool.
    
    The validation query was obtained from the MySQL reference manual:
    http://dev.mysql.com/doc/refman/5.1/en/connector-j-usagenotes-j2ee.html
    "/* ping */ SELECT 1"
    
    Testing on borrow will add slight overhead for requests needing a database
    connection as the validation query needs to execute before the connection is
    returned by the pool.  testOnBorrow is the best option provided by DBCP.
    
    Over time, we should evaluate alternative connection pools.  For example: the
    Tomcat connection pool introduced alongside Tomcat 7 (also compatible with
    Tomcat 6) offers higher concurrency within the pool, and has more/better
    options for validating connections.
    
    Change-Id: Ie57708d9b88d0b1f930d634db3f7fc04d2187d1e

--- cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/relational/MysqlServiceCreator.java
+++ cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/relational/MysqlServiceCreator.java
@@ -19,7 +19,13 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 public class MysqlServiceCreator 
     extends AbstractServiceCreator<DataSource, MysqlServiceInfo> {
 	
-	private static String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+	private static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+	
+	/**
+	 * Validation query obtained from the MySQL reference manual:
+	 * http://dev.mysql.com/doc/refman/5.1/en/connector-j-usagenotes-j2ee.html
+	 */
+	private static final String VALIDATION_QUERY = "/* ping */ SELECT 1";
 	
 	public MysqlServiceCreator(CloudEnvironment cloudEnvironment) {
 		super(cloudEnvironment, MysqlServiceInfo.class);
@@ -29,7 +35,15 @@ public class MysqlServiceCreator
 	 * Create a datasource based on service info.
 	 * 
 	 * <p>
-	 * Tries to create a pooled connection based on either 
+	 * Tries to create a pooled connection based on either Commons DBCP or
+	 * Tomcat's internal DBCP.  The connection pool will validate connections
+	 * as they are dispensed.
+	 * 
+	 * <p>
+	 * Testing on borrow will add slight overhead for requests needing a
+	 * database connection as the validation query needs to execute before the
+	 * connection is returned by the pool.  testOnBorrow is the best option
+	 * provided by DBCP.
 	 */
 	public DataSource createService(MysqlServiceInfo serviceInfo) {
 		try {
@@ -41,6 +55,8 @@ public class MysqlServiceCreator
 				ds.setUrl(serviceInfo.getUrl());
 				ds.setUsername(serviceInfo.getUserName());
 				ds.setPassword(serviceInfo.getPassword());
+				ds.setValidationQuery(VALIDATION_QUERY);
+				ds.setTestOnBorrow(true);
 				return ds;
 				// else, we have one from Tomcat
 			} else if (hasClass("org.apache.tomcat.dbcp.dbcp.BasicDataSource")) {
@@ -49,6 +65,8 @@ public class MysqlServiceCreator
 				ds.setUrl(serviceInfo.getUrl());
 				ds.setUsername(serviceInfo.getUserName());
 				ds.setPassword(serviceInfo.getPassword());
+				ds.setValidationQuery(VALIDATION_QUERY);
+				ds.setTestOnBorrow(true);
 				return ds;
 			} else {
 				// Only for testing outside Tomcat/CloudFoundry
