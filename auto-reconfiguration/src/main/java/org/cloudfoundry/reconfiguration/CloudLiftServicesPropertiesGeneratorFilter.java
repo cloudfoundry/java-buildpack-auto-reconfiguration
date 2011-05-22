2d7e2ccd23dfca3bcb4aaf024263a608be3b0138
AB Srinivasan <abyss@b20nine.com>
2011-05-22 01:03:02 -0700

    Lift support
    Change-Id: I7a4c046b38d417e8aeb01b4df4ca748e3673ce82

---
+++ auto-reconfiguration/src/main/java/org/cloudfoundry/reconfiguration/CloudLiftServicesPropertiesGeneratorFilter.java
@@ -0,0 +1,121 @@
+// Copyright (c) 2009-2011 VMware, Inc.
+
+package org.cloudfoundry.reconfiguration;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.cloudfoundry.runtime.env.CloudEnvironment;
+import org.cloudfoundry.runtime.env.MysqlServiceInfo;
+
+/**
+ * A servlet filter implementation that is used to generate a property file
+ * with service credentials for use by Lift web applications.
+ *
+ * @author A.B.Srinivasan
+ *
+ */
+public class CloudLiftServicesPropertiesGeneratorFilter implements Filter {
+
+    static final String PROPERTIES_FILE_PATH =
+        "webapps/ROOT/WEB-INF/classes/props/";
+    static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+    static final String PROPERTIES_FILE_SUFFIX = ".props";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        CloudEnvironment cloudEnvironment = new CloudEnvironment();
+        try {
+            generatePropertiesFile(getPropertiesFileName(), cloudEnvironment);
+        } catch (IOException ioe) {
+            throw new ServletException("Lift services properties file " +
+                "generation failed: ", ioe.getCause());
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    void generatePropertiesFile(String propertiesFile,
+        CloudEnvironment cloudEnvironment)
+        throws ServletException, IOException {
+        Properties props = generateDBServiceProperties(cloudEnvironment);
+        if (!props.isEmpty()) {
+            createPropertiesFile(propertiesFile);
+            props.store(new FileOutputStream(propertiesFile), null);
+        }
+    }
+
+    String getPropertiesFileName() throws IOException {
+        // The properties file naming follows the "<username>.<hostname>.props"
+        // pattern as per the conventions laid out in net.liftweb.util.Props
+        String username = System.getProperty("user.name");
+        String hostname = InetAddress.getLocalHost().getHostName();
+        return PROPERTIES_FILE_PATH + username + "." +
+            hostname + PROPERTIES_FILE_SUFFIX;
+    }
+
+    void createPropertiesFile(String propertiesFileName) throws IOException {
+        File propertiesFile = new File(propertiesFileName);
+        if (propertiesFile.exists()) {
+        	return;
+        }
+        File parentPath = new File(PROPERTIES_FILE_PATH);
+        if (!parentPath.exists()) {
+            if (!parentPath.mkdirs()) {
+                throw new IOException(
+                    "Failed to create parent directory '" +
+                    PROPERTIES_FILE_PATH +
+                    "' for properties file");
+            }
+        }
+        if (!propertiesFile.createNewFile()) {
+            throw new IOException(
+                "Failed to create properties file '" +
+                propertiesFileName + "'");
+        }
+    }
+
+    Properties generateDBServiceProperties(CloudEnvironment cloudEnvironment) throws ServletException {
+        List<MysqlServiceInfo> dbServiceInfos =
+            cloudEnvironment.getServiceInfos(MysqlServiceInfo.class);
+        Properties props = new Properties();
+        if (dbServiceInfos.size() == 0) {
+            return props;
+        } else {
+            // Note: Currently, the following is very specific to the
+            // MySql service and works only if there is one DB instance
+            // associated with the application.
+            if (dbServiceInfos.size() == 1) {
+                MysqlServiceInfo serviceInfo = dbServiceInfos.get(0);
+                props.setProperty("db.class", DRIVER_CLASS_NAME);
+                props.setProperty("db.url", serviceInfo.getUrl());
+                props.setProperty("db.user", serviceInfo.getUserName());
+                props.setProperty("db.pass", serviceInfo.getPassword());
+                return props;
+            } else {
+                throw new ServletException(
+                    "Lift services properties file generation failed: " +
+                    "application has more than one DB instance bound to it");
+            }
+        }
+    }
+}
---
+++ auto-reconfiguration/src/test/java/org/cloudfoundry/reconfiguration/CloudLiftServicesPropertiesGeneratorFilterTest.java
@@ -0,0 +1,182 @@
+package org.cloudfoundry.reconfiguration;
+
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+
+import org.cloudfoundry.runtime.env.CloudEnvironment;
+import org.cloudfoundry.runtime.env.MysqlServiceInfo;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Unit-tests for {@link CloudLiftServicesPropertiesGeneratorFilter}.
+ *
+ * @author A.B.Srinivasan
+ *
+ */
+public class CloudLiftServicesPropertiesGeneratorFilterTest {
+    @Mock private MysqlServiceInfo mockMysqlServiceInfo1;
+    @Mock private MysqlServiceInfo mockMysqlServiceInfo2;
+    @Mock private CloudEnvironment mockEnvironment;
+
+    private CloudLiftServicesPropertiesGeneratorFilter
+        testLiftPropertiesGeneratorFilter;
+    private String fileName = "/tmp/cloudLiftServicesProperties.props";
+    private String serviceJdbcUrl = "jdbc:mysql://10.20.20.40:1234/mysql-1";
+    private String serviceUserName = "testUser";
+    private String servicePassword = "testPassword";
+
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        testLiftPropertiesGeneratorFilter =
+            new CloudLiftServicesPropertiesGeneratorFilter();
+    }
+
+    @After
+    public void tearDown() {
+        File file = new File(fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void cloudLiftServicesPropertiesFileNamedCorrectly()
+    throws IOException {
+        String propertiesFileName = null;
+        String generatedName = null;
+        propertiesFileName =
+            CloudLiftServicesPropertiesGeneratorFilter.PROPERTIES_FILE_PATH +
+            System.getProperty("user.name") + "." +
+            InetAddress.getLocalHost().getHostName() +
+            CloudLiftServicesPropertiesGeneratorFilter.PROPERTIES_FILE_SUFFIX;
+
+        generatedName = testLiftPropertiesGeneratorFilter.getPropertiesFileName();
+
+        Assert.assertNotNull(propertiesFileName);
+        Assert.assertNotNull(generatedName);
+        Assert.assertEquals(propertiesFileName, generatedName);
+    }
+
+    @Test
+    public void cloudLiftServicesPropertiesGeneratorCreatesFilesCorrectly()
+    throws IOException {
+        testLiftPropertiesGeneratorFilter.createPropertiesFile(fileName);
+        Assert.assertTrue(new File(fileName).exists());
+    }
+
+    @Test (expected=ServletException.class)
+    public void cloudLiftServicesPropertiesGenerationFailsIfMultipleMysqlServicesPresent()
+    throws ServletException {
+        setUpMultipleMysqlServicesMock();
+
+        Properties props = testLiftPropertiesGeneratorFilter.generateDBServiceProperties(
+            mockEnvironment);
+    }
+
+    @Test
+    public void cloudLiftServicesPropertiesNotGeneratedIfNoMysqlServicePresent()
+    throws IOException, ServletException {
+        setUpNoMysqlServiceMock();
+
+        Properties props = testLiftPropertiesGeneratorFilter.generateDBServiceProperties(
+            mockEnvironment);
+
+        Assert.assertTrue(props.isEmpty());
+    }
+
+    @Test
+    public void cloudLiftServicesPropertiesFileNotCreatedIfNoMysqlServicePresent()
+    throws IOException, ServletException {
+        setUpNoMysqlServiceMock();
+
+        testLiftPropertiesGeneratorFilter.generatePropertiesFile(
+                fileName, mockEnvironment);
+
+        Assert.assertFalse(new File(fileName).exists());
+    }
+
+    @Test
+    public void cloudLiftServicesPropertiesGeneratedCorrectlyForMysqlServiceIfOnePresent()
+    throws IOException, ServletException {
+        setUpSingleMysqlServiceMock();
+
+        Properties props = testLiftPropertiesGeneratorFilter.generateDBServiceProperties(
+            mockEnvironment);
+
+        Assert.assertEquals(
+            CloudLiftServicesPropertiesGeneratorFilter.DRIVER_CLASS_NAME,
+            props.getProperty("db.class"));
+        Assert.assertEquals(serviceJdbcUrl, props.getProperty("db.url"));
+        Assert.assertEquals(serviceUserName, props.getProperty("db.user"));
+        Assert.assertEquals(servicePassword, props.getProperty("db.pass"));
+    }
+
+    @Test
+    public void cloudLiftServicesPropertiesFileGeneratedCorrectlyForMysqlServiceIfOnePresent()
+    throws ServletException, IOException {
+        setUpSingleMysqlServiceMock();
+
+        testLiftPropertiesGeneratorFilter.generatePropertiesFile(
+            fileName, mockEnvironment);
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(fileName));
+
+        Assert.assertEquals(
+            CloudLiftServicesPropertiesGeneratorFilter.DRIVER_CLASS_NAME,
+            properties.getProperty("db.class"));
+        Assert.assertEquals(serviceJdbcUrl, properties.getProperty("db.url"));
+        Assert.assertEquals(serviceUserName, properties.getProperty("db.user"));
+        Assert.assertEquals(servicePassword, properties.getProperty("db.pass"));
+    }
+
+    private void setUpNoMysqlServiceMock() {
+        List<MysqlServiceInfo> serviceInfos = new ArrayList<MysqlServiceInfo>();
+        when(mockEnvironment.getServiceInfos(MysqlServiceInfo.class)).
+            thenReturn(serviceInfos);
+    }
+
+    private void setUpSingleMysqlServiceMock() {
+        List<MysqlServiceInfo> serviceInfos = new ArrayList<MysqlServiceInfo>();
+
+        serviceInfos.add(mockMysqlServiceInfo1);
+        when(mockMysqlServiceInfo1.getUrl()).thenReturn(serviceJdbcUrl);
+        when(mockMysqlServiceInfo1.getUserName()).thenReturn(serviceUserName);
+        when(mockMysqlServiceInfo1.getPassword()).thenReturn(servicePassword);
+
+        when(mockEnvironment.getServiceInfos(MysqlServiceInfo.class)).
+            thenReturn(serviceInfos);
+    }
+
+    private void setUpMultipleMysqlServicesMock() {
+        List<MysqlServiceInfo> serviceInfos = new ArrayList<MysqlServiceInfo>();
+
+        serviceInfos.add(mockMysqlServiceInfo1);
+        when(mockMysqlServiceInfo1.getUrl()).thenReturn(serviceJdbcUrl);
+        when(mockMysqlServiceInfo1.getUserName()).thenReturn(serviceUserName);
+        when(mockMysqlServiceInfo1.getPassword()).thenReturn(servicePassword);
+
+        serviceInfos.add(mockMysqlServiceInfo2);
+        when(mockMysqlServiceInfo2.getUrl()).thenReturn(serviceJdbcUrl);
+        when(mockMysqlServiceInfo2.getUserName()).thenReturn(serviceUserName);
+        when(mockMysqlServiceInfo2.getPassword()).thenReturn(servicePassword);
+
+        when(mockEnvironment.getServiceInfos(MysqlServiceInfo.class)).
+            thenReturn(serviceInfos);
+    }
+}
