e30a99406d5eca5b7670b913e98e7f600d8fe333
David Wragg <david@rabbitmq.com>
2011-06-02 11:13:45 +0100

    Add support for the RabbitMQ service under multiple labels
    Change-Id: I51b023d2d532e1944b4b71888bd87fae2b5eaa8e

--- cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/env/CloudEnvironment.java
+++ cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/env/CloudEnvironment.java
@@ -3,8 +3,10 @@ package org.cloudfoundry.runtime.env;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -22,14 +24,32 @@ public class CloudEnvironment {
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 	private EnvironmentAccessor environment = new EnvironmentAccessor();
-	
-	private static Map<Class<? extends AbstractServiceInfo>, String> serviceTypeToNameMap = new HashMap<Class<? extends AbstractServiceInfo>, String>();
-	
+
+	private static Map<Class<? extends AbstractServiceInfo>, Set<String>> serviceTypeToLabels = new HashMap<Class<? extends AbstractServiceInfo>, Set<String>>();
+
+	private static void labelledServiceType(Class<? extends AbstractServiceInfo> serviceType,
+				      String label)
+	{
+		Set<String> labels = serviceTypeToLabels.get(serviceType);
+		if (labels == null) {
+			labels = new HashSet<String>();
+			serviceTypeToLabels.put(serviceType, labels);
+		}
+
+		labels.add(label);
+	}
+
 	static {
-		serviceTypeToNameMap.put(MysqlServiceInfo.class, "mysql-5.1");
-		serviceTypeToNameMap.put(RedisServiceInfo.class, "redis-2.2");
-		serviceTypeToNameMap.put(MongoServiceInfo.class, "mongodb-1.8");
-		serviceTypeToNameMap.put(RabbitServiceInfo.class, "rabbitmq-2.4");
+		labelledServiceType(MysqlServiceInfo.class, "mysql-5.1");
+		labelledServiceType(RedisServiceInfo.class, "redis-2.2");
+		labelledServiceType(MongoServiceInfo.class, "mongodb-1.8");
+
+                // the old rabbitmq service
+		labelledServiceType(RabbitServiceInfo.class, "rabbitmq-2.4");
+                // rabbitmq SRS-based service, during testing
+		labelledServiceType(RabbitServiceInfo.class, "rabbitmq-srs-2.4.1");
+                // rabbitmq SRS-based service, after the old service is gone
+		labelledServiceType(RabbitServiceInfo.class, "rabbitmq-2.4.1");
 	}
 	
 	/* package for testing purpose */
@@ -107,51 +127,42 @@ public class CloudEnvironment {
 		}
 		return null;
 	}
-	
-	private List<Map<String, Object>> getServiceDataByServiceType(String type) {
+
+	private List<Map<String, Object>> getServiceDataLabelled(Set<String> labels) {
 		List<Map<String, Object>> services = getServices();
-		
 		List<Map<String, Object>> matchedServices = new ArrayList<Map<String,Object>>();
-		
-		for (Map<String, Object> service : services) {
-			if (service.get("label").equals(type)) {
+
+		for (Map<String, Object> service : services)
+			if (labels.contains(service.get("label")))
 				matchedServices.add(service);
-			}
-		}
+
 		return matchedServices;
 	}
-	
-	
+
 	public <T extends AbstractServiceInfo> T getServiceInfo(String name, Class<T> serviceInfoType) {
 		Map<String,Object> serviceInfoMap = getServiceDataByName(name);
-		String typeName = serviceTypeToNameMap.get(serviceInfoType);
-		
-		if (serviceInfoMap.get("label").equals(typeName)) {
+		Set<String> labels = serviceTypeToLabels.get(serviceInfoType);
+
+		if (labels != null && labels.contains(serviceInfoMap.get("label")))
 			return getServiceInfo(serviceInfoMap, serviceInfoType);
-		}
-		return null;
+		else
+			return null;
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public List<? extends AbstractServiceInfo> getServiceInfos() {
-		List serviceInfos = new ArrayList();
-		for (Map.Entry<Class<? extends AbstractServiceInfo>, String> serviceTypeToNameEntry : serviceTypeToNameMap.entrySet()) {
-			serviceInfos.addAll(getServiceInfos(serviceTypeToNameEntry.getKey()));
-		}
-		return serviceInfos;
-	}
-	
 	public <T extends AbstractServiceInfo> List<T> getServiceInfos(Class<T> serviceInfoType) {
-		String typeName = serviceTypeToNameMap.get(serviceInfoType);
-		List<Map<String,Object>> serviceInfoMaps = getServiceDataByServiceType(typeName);
-		
+		Set<String> labels = serviceTypeToLabels.get(serviceInfoType);
 		List<T> serviceInfos = new ArrayList<T>();
-		for (Map<String,Object> serviceInfoMap : serviceInfoMaps) {
-			serviceInfos.add(getServiceInfo(serviceInfoMap, serviceInfoType));
+
+		if (labels != null) {
+			List<Map<String,Object>> serviceInfoMaps = getServiceDataLabelled(labels);
+
+			for (Map<String,Object> serviceInfoMap : serviceInfoMaps)
+				serviceInfos.add(getServiceInfo(serviceInfoMap, serviceInfoType));
 		}
+
 		return serviceInfos;
 	}
-	
+
 	private <T extends AbstractServiceInfo> T getServiceInfo(Map<String,Object> serviceInfoMap, Class<T> serviceInfoType) {
 		try {
 			Constructor<T> ctor = serviceInfoType.getConstructor(Map.class);
