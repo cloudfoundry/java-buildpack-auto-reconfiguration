b465f0a698237166252d86dddd7af5a292f15461
Ramnivas Laddad <ramnivas@ramnivas.com>
2011-07-27 21:21:31 -0700

    Added CloudMongoDbFactoryBean and CloudMongoDbFactoryTest missing from earlier push
    Change-Id: If0eae6a940fa1227b05b17e6022aa23ec3f51980

---
+++ cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/document/CloudMongoDbFactoryBean.java
@@ -0,0 +1,29 @@
+package org.cloudfoundry.runtime.service.document;
+
+import org.cloudfoundry.runtime.env.CloudEnvironment;
+import org.cloudfoundry.runtime.env.MongoServiceInfo;
+import org.cloudfoundry.runtime.service.AbstractCloudServiceFactory;
+import org.springframework.data.document.mongodb.MongoDbFactory;
+
+import com.mongodb.Mongo;
+
+/**
+ * Spring factory bean for Mongo service.
+ *
+ * @author Ramnivas Laddad
+ *
+ */
+public class CloudMongoDbFactoryBean extends AbstractCloudServiceFactory<MongoDbFactory, MongoServiceInfo> {
+	public CloudMongoDbFactoryBean(CloudEnvironment cloudEnvironment) {
+		super(new MongoServiceCreator(cloudEnvironment));
+	}
+
+	public CloudMongoDbFactoryBean() {
+		super(new MongoServiceCreator(new CloudEnvironment()));
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return Mongo.class;
+	}
+}
