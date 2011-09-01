cb6380824a6ce9a500a206aac3f2d83f07ca621c
Jennifer Hickey <jhickey@vmware.com>
2011-08-31 18:04:23 -0700

    Added test app for cloudfoundry-runtime and auto-reconfiguration integration testing
    Change-Id: Id0db74fc79f2b3804d2b27a199368bf85896140c

---
+++ vcap-java-test-app/src/main/java/org/cloudfoundry/test/ServiceController.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cloudfoundry.test;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Controller that exposes basic information about dependencies that were
+ * auto-wired on app startup
+ *
+ * @author Jennifer Hickey
+ *
+ */
+@Controller
+public class ServiceController {
+
+	@Autowired
+	private ServiceHolder serviceHolder;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@ResponseBody
+	public String hello(HttpServletResponse response) throws IOException {
+		return "Welcome to the vcap-java test application";
+	}
+
+	@RequestMapping(value = "/properties", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseEntity<Properties> getServiceProperties() {
+		if (serviceHolder.getServiceProperties() == null) {
+			return new ResponseEntity<Properties>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Properties>(serviceHolder.getServiceProperties(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/mysql", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseEntity<String> getMySQLDataSourceDBUrl() {
+		if (serviceHolder.getMySqlDataSource() == null) {
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>(serviceHolder.getMySqlDataSource().getUrl(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/postgres", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseEntity<String> getPostgresDataSourceDBUrl() {
+		if (serviceHolder.getPostgresDataSource() == null) {
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>(serviceHolder.getPostgresDataSource().getUrl(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/mongo", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseEntity<String> getMongoClass() {
+		if (serviceHolder.getMongoDbFactory() == null) {
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>(serviceHolder.getMongoDbFactory().getClass().getName(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/rabbit", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseEntity<String> getRabbitClass() {
+		if (serviceHolder.getRabbitConnectionFactory() == null) {
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>(serviceHolder.getRabbitConnectionFactory().getClass().getName(),
+				HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/redis", method = RequestMethod.GET)
+	public @ResponseBody
+	ResponseEntity<String> getRedisClass() {
+		if (serviceHolder.getRedisConnectionFactory() == null) {
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<String>(serviceHolder.getRedisConnectionFactory().getClass().getName(), HttpStatus.OK);
+	}
+}
