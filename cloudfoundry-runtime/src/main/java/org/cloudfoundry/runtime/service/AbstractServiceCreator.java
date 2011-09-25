830419fa0e72d064b6c775cc54ccfbda30533a8c
Thomas Risberg <trisberg@vmware.com>
2011-09-25 09:49:50 -0400

    Added configuration attributes to the cloud namespace for data-source, mongo, redis and rabbit
    - Refactored the AbstractDataSourceCreator
    
    - Added namespace tests of the cloud namespace for data-source, mongo, redis and rabbit
    
    Change-Id: I64cd188833cbe429441464ce9fc1ce51a443b352

--- cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/AbstractServiceCreator.java
+++ cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/AbstractServiceCreator.java
@@ -1,9 +1,11 @@
 package org.cloudfoundry.runtime.service;
 
+import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.cloudfoundry.runtime.env.AbstractServiceInfo;
+import org.springframework.beans.BeanWrapper;
 
 /**
  * Base service creator
@@ -14,36 +16,37 @@ import org.cloudfoundry.runtime.env.AbstractServiceInfo;
  * <li>The service matching the given name</li>
  * <li>All services matching of the matching type</li>
  * </ul>
- * 
+ *
  * @author Ramnivas Laddad
  * @author Jennifer Hickey
+ * @author Thomas Risberg
  *
  * @param <S> Service type to be create
  * @param <SI> ServiceInfo type
  */
 public abstract class AbstractServiceCreator<S, SI extends AbstractServiceInfo> {
-	
+
 	/**
 	 * Implementation of this method must create service based on the service info object passed.
-	 * 
+	 *
 	 * @param serviceInfo
 	 * @return service
 	 */
 	public abstract S createService(SI serviceInfo);
-	
+
 	/**
 	 * Create service based on the unique service of the required type.
-	 * 
+	 *
 	 * @param singletonServiceInfo
 	 * @return service object along with the name of the matching service
 	 */
 	public ServiceNameTuple<S> createSingletonService(SI singletonServiceInfo) {
 		return new ServiceNameTuple<S>(createService(singletonServiceInfo), singletonServiceInfo.getServiceName());
 	}
-	
+
 	/**
 	 * Create service objects for all specified services
-	 * 
+	 *
 	 * @param serviceInfos
 	 * @return service objects along with the name of the matching services
 	 */
@@ -54,13 +57,34 @@ public abstract class AbstractServiceCreator<S, SI extends AbstractServiceInfo> 
 		}
 		return services;
 	}
-	
+
+	protected void setCorrespondingProperties(BeanWrapper target, BeanWrapper source) {
+		for (PropertyDescriptor pd : source.getPropertyDescriptors()) {
+			String property = pd.getName();
+			if (!"class".equals(property) && source.isReadableProperty(property) &&
+						source.getPropertyValue(property) != null) {
+				if (target.isWritableProperty(property)) {
+					target.setPropertyValue(property, source.getPropertyValue(property));
+				}
+			}
+		}
+	}
+
+	protected boolean hasClass(String name) {
+		try {
+			Class.forName(name);
+			return true;
+		} catch (Throwable ex) {
+			return false;
+		}
+	}
+
 	public static class ServiceNameTuple<S> {
 		public ServiceNameTuple(S service, String name) {
 			this.service = service;
 			this.name = name;
 		}
-		
+
 		public S service;
 		public String name;
 	}
--- cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/messaging/RabbitServiceCreator.java
+++ cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/service/messaging/RabbitServiceCreator.java
@@ -2,26 +2,37 @@ package org.cloudfoundry.runtime.service.messaging;
 
 import org.cloudfoundry.runtime.env.RabbitServiceInfo;
 import org.cloudfoundry.runtime.service.AbstractServiceCreator;
+import org.cloudfoundry.runtime.service.CloudRabbitConfiguration;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 /**
  * Simplified access to creating RabbitMQ service objects.
- * 
+ *
  * @author Ramnivas Laddad
  * @author Dave Syer
+ * @author Thomas Risberg
  *
  */
-public class RabbitServiceCreator 
+public class RabbitServiceCreator
 	extends AbstractServiceCreator<ConnectionFactory, RabbitServiceInfo> {
 
+	private CloudRabbitConfiguration cloudRabbitConfiguration;
+
+	public void setCloudRabbitConfiguration(CloudRabbitConfiguration cloudRabbitConfiguration) {
+		this.cloudRabbitConfiguration = cloudRabbitConfiguration;
+	}
+
 	public ConnectionFactory createService(RabbitServiceInfo serviceInfo) {
-		CachingConnectionFactory connectionFactory 
+		CachingConnectionFactory connectionFactory
 		    = new CachingConnectionFactory(serviceInfo.getHost());
 		connectionFactory.setVirtualHost(serviceInfo.getVirtualHost());
 		connectionFactory.setUsername(serviceInfo.getUserName());
 		connectionFactory.setPassword(serviceInfo.getPassword());
 		connectionFactory.setPort(serviceInfo.getPort());
+		if (cloudRabbitConfiguration != null) {
+			connectionFactory.setChannelCacheSize(cloudRabbitConfiguration.getChannelCacheSize());
+		}
 		return connectionFactory;
 	}
 }
