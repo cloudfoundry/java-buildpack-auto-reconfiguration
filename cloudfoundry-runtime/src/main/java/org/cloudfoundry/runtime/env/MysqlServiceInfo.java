a95c3857859c2b109c1e6c2a0add89dfb1ac2aed
Miguel Pastor <miguelinlas3@gmail.com>
2011-11-09 19:26:55 +0100

    Implement the method to retrieve the driver classname in all the subclasses

--- cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/env/MysqlServiceInfo.java
+++ cloudfoundry-runtime/src/main/java/org/cloudfoundry/runtime/env/MysqlServiceInfo.java
@@ -12,12 +12,20 @@ import java.util.Map;
 
 public class MysqlServiceInfo extends AbstractDataSourceServiceInfo {
 
+	private static final String MYSQL_DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+
 	public MysqlServiceInfo(Map<String, Object> serviceInfo) {
 		super(serviceInfo);
+	}
+
+	@Override
+	public String getDriverClassName() {
+		return MYSQL_DRIVER_CLASS_NAME;
 	}
 
 	@Override
 	public String getUrl() {
 		return "jdbc:mysql://" + getHost() + ":" + + getPort() + "/" + getDatabase();
 	}
+
 }
