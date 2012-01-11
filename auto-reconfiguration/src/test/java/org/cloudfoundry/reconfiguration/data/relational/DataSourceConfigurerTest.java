4fb50235e2ac6ec55f3c8a6effe65e95fdaa78e9
Jens Deppe <jdeppe@vmware.com>
2012-01-11 14:16:57 -0800

    Sqlfire integration - round 2
    Change-Id: I22ad39f85ca5fdba25d692e3b73c188d3d2de72c

--- auto-reconfiguration/src/test/java/org/cloudfoundry/reconfiguration/data/relational/DataSourceConfigurerTest.java
+++ auto-reconfiguration/src/test/java/org/cloudfoundry/reconfiguration/data/relational/DataSourceConfigurerTest.java
@@ -28,65 +28,80 @@ import org.springframework.jdbc.datasource.AbstractDriverBasedDataSource;
  */
 public class DataSourceConfigurerTest extends CloudEnvironmentMockingTest {
 
-	@Mock
-	private RdbmsServiceInfo mockRdbmsServiceInfo;
+    @Mock
+    private RdbmsServiceInfo mockRdbmsServiceInfo;
 
-	@Mock
-	private RdbmsServiceInfo mockRdbmsServiceInfo2;
+    @Mock
+    private RdbmsServiceInfo mockRdbmsServiceInfo2;
 
-	@Test
-	public void cloudDataSourceReplacesUserDataSourceIfMySqlServiceDetected() {
-		String serviceJdbcUrl = "jdbc:mysql://10.20.20.40:1234/mysql-1";
-		List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
-		serviceInfos.add(mockRdbmsServiceInfo);
-		when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl);
-		when(mockRdbmsServiceInfo.getLabel()).thenReturn("mysql-5.1");
-		when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
+    @Test
+    public void cloudDataSourceReplacesUserDataSourceIfMySqlServiceDetected() {
+        String serviceJdbcUrl = "jdbc:mysql://10.20.20.40:1234/mysql-1";
+        List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
+        serviceInfos.add(mockRdbmsServiceInfo);
+        when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl);
+        when(mockRdbmsServiceInfo.getLabel()).thenReturn("mysql-5.1");
+        when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
 
-		ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
-		AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
-				DataSource.class);
-		Assert.assertEquals(serviceJdbcUrl, replacedDataSource.getUrl());
-	}
+        ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
+        AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
+                DataSource.class);
+        Assert.assertEquals(serviceJdbcUrl, replacedDataSource.getUrl());
+    }
 
-	@Test
-	public void cloudDataSourceReplacesUserDataSourceIfPostgreSqlServiceDetected() {
-		String serviceJdbcUrl = "jdbc:postgresql://10.20.20.40:5432/mydb-1";
-		List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
-		serviceInfos.add(mockRdbmsServiceInfo);
-		when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl);
-		when(mockRdbmsServiceInfo.getLabel()).thenReturn("postgresql-9.0");
-		when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
+    @Test
+    public void cloudDataSourceReplacesUserDataSourceIfPostgreSqlServiceDetected() {
+        String serviceJdbcUrl = "jdbc:postgresql://10.20.20.40:5432/mydb-1";
+        List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
+        serviceInfos.add(mockRdbmsServiceInfo);
+        when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl);
+        when(mockRdbmsServiceInfo.getLabel()).thenReturn("postgresql-9.0");
+        when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
 
-		ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
-		AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
-				DataSource.class);
-		Assert.assertEquals(serviceJdbcUrl, replacedDataSource.getUrl());
-	}
+        ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
+        AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
+                DataSource.class);
+        Assert.assertEquals(serviceJdbcUrl, replacedDataSource.getUrl());
+    }
 
-	@Test
-	public void cloudDataSourceLeavesOriginalInPlaceIfMultipleServicesDetected() {
-		String serviceJdbcUrl1 = "jdbc:mysql://10.20.20.40:1234/mysql-1";
-		String serviceJdbcUrl2 = "jdbc:postgresql://10.20.20.40:5432/pg-2";
-		List<AbstractDataSourceServiceInfo> serviceInfos = new ArrayList<AbstractDataSourceServiceInfo>();
-		serviceInfos.add(mockRdbmsServiceInfo);
-		serviceInfos.add(mockRdbmsServiceInfo2);
-		when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl1);
-		when(mockRdbmsServiceInfo2.getUrl()).thenReturn(serviceJdbcUrl2);
-		when(mockEnvironment.getServiceInfos(AbstractDataSourceServiceInfo.class)).thenReturn(serviceInfos);
-		ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
-		AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
-				DataSource.class);
-		Assert.assertEquals("jdbc:hsql:localdb", replacedDataSource.getUrl());
-	}
+    @Test
+    public void cloudDataSourceReplacesUserDataSourceIfSqlfireServiceDetected() {
+        String serviceJdbcUrl = "jdbc:sqlfire://10.20.20.40:5432/mydb-1";
+        List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
+        serviceInfos.add(mockRdbmsServiceInfo);
+        when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl);
+        when(mockRdbmsServiceInfo.getLabel()).thenReturn("sqlfire-1.0");
+        when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
 
-	@Test
-	public void cloudDataSourceLeavesOriginalInPlaceIfNoServicesDetected() {
-		List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
-		when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
-		ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
-		AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
-				DataSource.class);
-		Assert.assertEquals("jdbc:hsql:localdb", replacedDataSource.getUrl());
-	}
+        ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
+        AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
+                DataSource.class);
+        Assert.assertEquals(serviceJdbcUrl, replacedDataSource.getUrl());
+    }
+
+    @Test
+    public void cloudDataSourceLeavesOriginalInPlaceIfMultipleServicesDetected() {
+        String serviceJdbcUrl1 = "jdbc:mysql://10.20.20.40:1234/mysql-1";
+        String serviceJdbcUrl2 = "jdbc:postgresql://10.20.20.40:5432/pg-2";
+        List<AbstractDataSourceServiceInfo> serviceInfos = new ArrayList<AbstractDataSourceServiceInfo>();
+        serviceInfos.add(mockRdbmsServiceInfo);
+        serviceInfos.add(mockRdbmsServiceInfo2);
+        when(mockRdbmsServiceInfo.getUrl()).thenReturn(serviceJdbcUrl1);
+        when(mockRdbmsServiceInfo2.getUrl()).thenReturn(serviceJdbcUrl2);
+        when(mockEnvironment.getServiceInfos(AbstractDataSourceServiceInfo.class)).thenReturn(serviceInfos);
+        ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
+        AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
+                DataSource.class);
+        Assert.assertEquals("jdbc:hsql:localdb", replacedDataSource.getUrl());
+    }
+
+    @Test
+    public void cloudDataSourceLeavesOriginalInPlaceIfNoServicesDetected() {
+        List<RdbmsServiceInfo> serviceInfos = new ArrayList<RdbmsServiceInfo>();
+        when(mockEnvironment.getServiceInfos(RdbmsServiceInfo.class)).thenReturn(serviceInfos);
+        ApplicationContext context = getTestApplicationContext("test-ds-good-context.xml");
+        AbstractDriverBasedDataSource replacedDataSource = (AbstractDriverBasedDataSource) context.getBean("myDs",
+                DataSource.class);
+        Assert.assertEquals("jdbc:hsql:localdb", replacedDataSource.getUrl());
+    }
 }
