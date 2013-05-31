8f6077e50f65da7a1d89b019fed4de1600f4fcdb
Ramnivas Laddad <rladdad@gopivotal.com>
2013-05-31 11:46:04 -0700

    Support <cloud:data-source> for rds-mysql and cleardb services
    Change-Id: I477445c935f5e7b89a7d1744e0aad1876576f036

--- cloudfoundry-runtime/src/test/java/org/cloudfoundry/runtime/service/config/xml/CloudDataSourceFactoryParserTest.java
+++ cloudfoundry-runtime/src/test/java/org/cloudfoundry/runtime/service/config/xml/CloudDataSourceFactoryParserTest.java
@@ -27,42 +27,54 @@ public class CloudDataSourceFactoryParserTest extends AbstractCloudParserTest {
 	@Test
 	public void testMySqlDataSourceDefinition() throws Exception {
 		Map<String, String> envMap = new HashMap<String, String>();
-		envMap.put("VCAP_SERVICES", CloudEnvironmentTestHelper.getServicesPayload(
-				new String[] {CloudEnvironmentTestHelper.getMysqlServicePayload("5.1", "mysql", "127.0.0.1", 3306,
-						"uIKpwGOTgjfaQ", "pu0xEgiy997kQ", "d855c4a2468de494984f94e85255ce5d5")},
-				new String[] {},
-				new String[] {},
-				new String[] {}));
-		envMap.put("VCAP_APPLICATION", CloudEnvironmentTestHelper.getApplicationInstanceInfo("test", "test.vcap.me"));
-		replaceCloudEnvironmentAccessor(envMap);
-		loadContext("CloudDataSourceFactoryParserTest-context.xml", CloudDataSourceFactoryParserTest.class,
-				appContext, cloudParser, "data-source");
+		String username = "uIKpwGOTgjfaQ";
+		String password = "pu0xEgiy997kQ";
+		String dbName = "d855c4a2468de494984f94e85255ce5d5";
+		String mysqlServicePayload = CloudEnvironmentTestHelper.getMysqlServicePayload("5.1", "mysql", "127.0.0.1", 3306,
+				username, password, dbName);
+		String rdsServicePayload = CloudEnvironmentTestHelper.getRdsServicePayload("n/a", "rds-mysql", "127.0.0.1", 3306,
+				username, password, dbName);
+		String cleardbServicePayload = CloudEnvironmentTestHelper.getCleardbServicePayload("n/a", "cleardb-dev", "127.0.0.1", 3306,
+				username, password, dbName);
+		String[] payloads = new String[]{mysqlServicePayload, rdsServicePayload, cleardbServicePayload};
 
-		Object beanWithPoolAndConnProps = this.appContext.getBean("dataSourceWithPoolAndConnProps");
-		assertEquals("not the correct class", "org.apache.commons.dbcp.BasicDataSource", beanWithPoolAndConnProps.getClass().getName());
-		BeanWrapper dataSourceWithPoolAndConnProps = new BeanWrapperImpl(beanWithPoolAndConnProps);
-		assertEquals("not the correct driverClassName", "com.mysql.jdbc.Driver",
-				dataSourceWithPoolAndConnProps.getPropertyValue("driverClassName"));
-		assertEquals("not the correct URL", "jdbc:mysql://127.0.0.1:3306/d855c4a2468de494984f94e85255ce5d5",
-				dataSourceWithPoolAndConnProps.getPropertyValue("url"));
-		assertEquals("not the correct username", "uIKpwGOTgjfaQ", dataSourceWithPoolAndConnProps.getPropertyValue("username"));
-		assertEquals("not the correct password", "pu0xEgiy997kQ", dataSourceWithPoolAndConnProps.getPropertyValue("password"));
-		assertEquals("not the correct validation-query", "/* ping */ SELECT 1", dataSourceWithPoolAndConnProps.getPropertyValue("validationQuery"));
-		assertEquals("not the correct test-on-borrow", true, dataSourceWithPoolAndConnProps.getPropertyValue("testOnBorrow"));
-		assertEquals("not the correct upper limit for pool-size", 20, dataSourceWithPoolAndConnProps.getPropertyValue("maxActive"));
-		assertEquals("not the correct lower limit for pool-size", 5, dataSourceWithPoolAndConnProps.getPropertyValue("initialSize"));
-		assertEquals("not the correct max-wait", 5000L, dataSourceWithPoolAndConnProps.getPropertyValue("maxWait"));
-		Properties theConnectionProperties = (Properties) ReflectionTestUtils.getField(beanWithPoolAndConnProps, "connectionProperties");
-		assertEquals("not the correct sessionVariables connection property", "sql_mode='ANSI'", theConnectionProperties.get("sessionVariables"));
-		assertEquals("not the correct characterEncoding connection property", "'UTF8'", theConnectionProperties.get("characterEncoding"));
+		for (String payload: payloads) {
+			envMap.put("VCAP_SERVICES", CloudEnvironmentTestHelper.getServicesPayload(
+					new String[] {payload},
+					new String[] {},
+					new String[] {},
+					new String[] {}));
+			envMap.put("VCAP_APPLICATION", CloudEnvironmentTestHelper.getApplicationInstanceInfo("test", "test.vcap.me"));
+			replaceCloudEnvironmentAccessor(envMap);
+			loadContext("CloudDataSourceFactoryParserTest-context.xml", CloudDataSourceFactoryParserTest.class,
+					appContext, cloudParser, "data-source");
 
-		Object beanWithDefaults = this.appContext.getBean("dataSourceWithDefaults");
-		assertEquals("not the correct class", "org.apache.commons.dbcp.BasicDataSource", beanWithDefaults.getClass().getName());
-		BeanWrapper dataSourceWithDefaults = new BeanWrapperImpl(beanWithDefaults);
-		assertEquals("not the correct maxActive", 8, dataSourceWithDefaults.getPropertyValue("maxActive"));
-		assertEquals("not the correct initialSize", 0, dataSourceWithDefaults.getPropertyValue("initialSize"));
-		assertEquals("not the correct maxWait", -1L, dataSourceWithDefaults.getPropertyValue("maxWait"));
-		Properties emptyConnectionProperties = (Properties) ReflectionTestUtils.getField(beanWithDefaults, "connectionProperties");
-		assertEquals("not empty connection properties", 0, emptyConnectionProperties.size());
+			Object beanWithPoolAndConnProps = this.appContext.getBean("dataSourceWithPoolAndConnProps");
+			assertEquals("not the correct class", "org.apache.commons.dbcp.BasicDataSource", beanWithPoolAndConnProps.getClass().getName());
+			BeanWrapper dataSourceWithPoolAndConnProps = new BeanWrapperImpl(beanWithPoolAndConnProps);
+			assertEquals("not the correct driverClassName", "com.mysql.jdbc.Driver",
+					dataSourceWithPoolAndConnProps.getPropertyValue("driverClassName"));
+			assertEquals("not the correct URL", "jdbc:mysql://127.0.0.1:3306/d855c4a2468de494984f94e85255ce5d5",
+					dataSourceWithPoolAndConnProps.getPropertyValue("url"));
+			assertEquals("not the correct username", username, dataSourceWithPoolAndConnProps.getPropertyValue("username"));
+			assertEquals("not the correct password", password, dataSourceWithPoolAndConnProps.getPropertyValue("password"));
+			assertEquals("not the correct validation-query", "/* ping */ SELECT 1", dataSourceWithPoolAndConnProps.getPropertyValue("validationQuery"));
+			assertEquals("not the correct test-on-borrow", true, dataSourceWithPoolAndConnProps.getPropertyValue("testOnBorrow"));
+			assertEquals("not the correct upper limit for pool-size", 20, dataSourceWithPoolAndConnProps.getPropertyValue("maxActive"));
+			assertEquals("not the correct lower limit for pool-size", 5, dataSourceWithPoolAndConnProps.getPropertyValue("initialSize"));
+			assertEquals("not the correct max-wait", 5000L, dataSourceWithPoolAndConnProps.getPropertyValue("maxWait"));
+			Properties theConnectionProperties = (Properties) ReflectionTestUtils.getField(beanWithPoolAndConnProps, "connectionProperties");
+			assertEquals("not the correct sessionVariables connection property", "sql_mode='ANSI'", theConnectionProperties.get("sessionVariables"));
+			assertEquals("not the correct characterEncoding connection property", "'UTF8'", theConnectionProperties.get("characterEncoding"));
+
+			Object beanWithDefaults = this.appContext.getBean("dataSourceWithDefaults");
+			assertEquals("not the correct class", "org.apache.commons.dbcp.BasicDataSource", beanWithDefaults.getClass().getName());
+			BeanWrapper dataSourceWithDefaults = new BeanWrapperImpl(beanWithDefaults);
+			assertEquals("not the correct maxActive", 8, dataSourceWithDefaults.getPropertyValue("maxActive"));
+			assertEquals("not the correct initialSize", 0, dataSourceWithDefaults.getPropertyValue("initialSize"));
+			assertEquals("not the correct maxWait", -1L, dataSourceWithDefaults.getPropertyValue("maxWait"));
+			Properties emptyConnectionProperties = (Properties) ReflectionTestUtils.getField(beanWithDefaults, "connectionProperties");
+			assertEquals("not empty connection properties", 0, emptyConnectionProperties.size());
+		}
 	}
 }
