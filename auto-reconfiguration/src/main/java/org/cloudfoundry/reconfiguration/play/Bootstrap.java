4405c78074a7ab4ecee6d2dbf701953a97442741
Glyn Normington <gnormington@gopivotal.com>
2013-07-05 17:01:01 +0100

    Create a Play plugin to auto-reconfigure Play apps
    To reconfigure a Play application, an application would use a Global
    class which is driven by play.api.GlobalPlugin that is defined with
    order 10000 in a play.plugins file inside the Play framework. To
    achieve a similar effect to a Global class with a class name that will
    not conflict with an application class, define a plugin class and give
    it an order of 9999.
    
    Remove auto-reconfiguration code from Bootstrap but Retain Bootstrap
    while cloudfoundry-buildpack-java still uses it.
    
    [#52912355]

--- auto-reconfiguration/src/main/java/org/cloudfoundry/reconfiguration/play/Bootstrap.java
+++ auto-reconfiguration/src/main/java/org/cloudfoundry/reconfiguration/play/Bootstrap.java
@@ -1,12 +1,12 @@
 package org.cloudfoundry.reconfiguration.play;
 
-import org.cloudfoundry.runtime.env.CloudEnvironment;
-
 import play.core.server.NettyServer;
 
 /**
- * Wrapper that takes care of environment initialization and
- * auto-reconfiguration before starting the main Play class.
+ * Wrapper that starts the main Play class.
+ * <p />
+ * Environment initialization and auto-reconfiguration is now handled by PlayPlugin, but this class is retained
+ * to reduce the impact on cloudfoundry-buildpack-java.
  *
  * @author Jennifer Hickey
  *
@@ -14,8 +14,6 @@ import play.core.server.NettyServer;
 public class Bootstrap {
 
 	public static void main(String[] args) {
-		CloudEnvironment environment = new CloudEnvironment();
-		new Configurer(new AppConfiguration(environment), new PropertySetter(environment)).configure();
 		NettyServer.main(args);
 	}
 
