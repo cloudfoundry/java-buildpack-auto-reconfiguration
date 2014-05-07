0d12bde6121dcda8a8ffc36b25a504a0c9664713
Ben Hale <bhale@gopivotal.com>
2014-05-07 12:37:09 +0100

    v1.1.0.BUILD-SNAPSHOT

--- auto-reconfiguration/src/main/groovy/org/cloudfoundry/reconfiguration/grails/JavaBuildpackAutoReconfigurationGrailsPlugin.groovy
+++ auto-reconfiguration/src/main/groovy/org/cloudfoundry/reconfiguration/grails/JavaBuildpackAutoReconfigurationGrailsPlugin.groovy
@@ -21,7 +21,7 @@ import org.cloudfoundry.reconfiguration.spring.CloudProfileApplicationContextIni
 import org.cloudfoundry.reconfiguration.spring.CloudPropertySourceApplicationContextInitializer
 
 class JavaBuildpackAutoReconfigurationGrailsPlugin {
-    def version = '1.0.0'
+    def version = '1.1.0.BUILD-SNAPSHOT'
 
     def doWithSpring = {
         def applicationContext = springConfig.unrefreshedApplicationContext
