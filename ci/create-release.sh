fb90a0ae46e9c1f504f06761eb9bffa83a406d94
Ben Hale <bhale@pivotal.io>
2017-04-21 15:15:14 -0700

    Create Release Polishing

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -5,13 +5,12 @@ set -e
 RELEASE=$1
 SNAPSHOT=$2
 
-mvn versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
+./mvnw versions:set -DnewVersion=$RELEASE -DgenerateBackupPoms=false
 git add .
 git commit --message "v$RELEASE Release"
-
 git tag -s v$RELEASE -m "v$RELEASE"
-git reset --hard HEAD^1
 
-mvn versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
+git reset --hard HEAD^1
+./mvnw versions:set -DnewVersion=$SNAPSHOT -DgenerateBackupPoms=false
 git add .
 git commit --message "v$SNAPSHOT Development"
